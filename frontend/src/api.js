f870169a43a0b6652c30c157254e77f266134f5b
prashant1s <prashantextra2003@gmail.com>
2026-04-16 01:50:48 +0530

    packq1

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
-const API = axios.create({ baseURL });
+const API = axios.create({ 
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
 export default API;
